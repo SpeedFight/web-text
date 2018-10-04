94d273619e829b783fd6220fefe791c7b50c36d5
SF <speedfight_2@wp.pl>
2018-10-04 19:44:49 +0200

    modyfying css

--- web-topic-client/src/app/app.module.ts
+++ web-topic-client/src/app/app.module.ts
@@ -4,11 +4,20 @@ import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
 import { TopicListComponent } from './components/topic-list/topic-list.component';
 import { NoteListComponent } from './components/note-list/note-list.component';
+import { MatTableModule, MatPaginatorModule, MatSortModule } from '@angular/material';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {MatGridListModule} from '@angular/material/grid-list';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MatToolbarModule} from '@angular/material/toolbar';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatInputModule} from '@angular/material';
+import {MatListModule} from '@angular/material/list';
+
 const appRoutes: Routes = [
   { path: 'topic/:id',      component: NoteListComponent },
   { path: '', component: TopicListComponent },
@@ -19,14 +28,24 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     TopicListComponent,
-    NoteListComponent
+    NoteListComponent,
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
     FormsModule,
     RouterModule.forRoot(appRoutes),
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatTableModule,
+    MatPaginatorModule,
+    MatSortModule,
+    MatGridListModule,
+    MatButtonModule,
+    MatCardModule,
+    MatToolbarModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatListModule
   ],
   exports: [
     RouterModule
