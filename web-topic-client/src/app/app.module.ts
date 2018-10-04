a7771bcef94966f8cf63c6b79508414694f6ea45
SF <speedfight_2@wp.pl>
2018-10-04 15:00:38 +0200

    fix pages for topic/note view and add note service

--- web-topic-client/src/app/app.module.ts
+++ web-topic-client/src/app/app.module.ts
@@ -2,21 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
 import { FormsModule } from '@angular/forms';
+import { RouterModule, Routes } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { TopicListComponent } from './components/topic-list/topic-list.component';
 import { TopicFormComponent } from './components/topic-form/topic-form.component';
+import { NoteListComponent } from './components/note-list/note-list.component';
+
+const appRoutes: Routes = [
+  { path: 'topic/:id',      component: NoteListComponent },
+  { path: '', component: TopicListComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   declarations: [
     AppComponent,
     TopicListComponent,
-    TopicFormComponent
+    TopicFormComponent,
+    NoteListComponent
   ],
   imports: [
     BrowserModule,
     HttpClientModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot(appRoutes)
+  ],
+  exports: [
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
